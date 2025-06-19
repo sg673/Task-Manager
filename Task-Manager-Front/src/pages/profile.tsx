293eb79b37d51c38f685394fc9fcea9be6b7bc83
sg673 <sg673@student.le.ac.uk>
2025-06-19 11:12:04 +0100

    Added the profile page and added a template

---
+++ Task-Manager-Front/src/pages/profile.tsx
@@ -0,0 +1,28 @@
+import { useEffect, useState } from "react";
+import { getUser } from "../services/api";
+import {User} from "../types/user";
+
+export default function Profile(){
+    const [user,setUser] = useState<User | null>(null);
+
+    useEffect(() => {
+        async function loadProfile(){
+            try{
+                const userData = await getUser();
+                setUser(userData);
+            }
+            catch(error){
+                console.error("Failed to load profile", error);
+            }
+        }
+
+        loadProfile();
+    },[]);
+    return(
+        <div className="max-w-4x1 mx-auto p-4">
+            <h1 className="text-2x1 font-bold mb-6">User Profile</h1>
+            
+            
+        </div>
+    );
+}
