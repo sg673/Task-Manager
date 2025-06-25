1d147cf8ed8a5a25d47f5811a06019c769ba4850
sg673 <sg673@student.le.ac.uk>
2025-06-25 12:27:54 +0100

    setup testing suite

--- Task-Manager-Front/vite.config.ts
+++ Task-Manager-Front/vite.config.ts
@@ -1,11 +1,17 @@
-import { defineConfig } from 'vite'
+import { defineConfig, type UserConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss(),
-  ],
-})
+    plugins: [
+      react(),
+      tailwindcss(),
+    ],
+    test : {
+      environment: 'jsdom',
+      globals: true,
+      setUpFiles: ['./src/__tests__/setup.ts'],
+    }
+  } as UserConfig
+)
