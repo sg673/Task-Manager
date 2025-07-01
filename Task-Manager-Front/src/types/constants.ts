4293db6de3de705e1947f335cb407156a53fff4a
sg673 <sg673@student.le.ac.uk>
2025-07-01 13:41:11 +0100

    added authorisation system

--- Task-Manager-Front/src/types/constants.ts
+++ Task-Manager-Front/src/types/constants.ts
@@ -1,2 +1,7 @@
+import { createContext } from "react";
+import { AuthContextType } from "../contexts/authContext";
+
 export const MAX_DESC_LENGTH = 85;
 export const MAX_TITLE_LENGTH = 30;
+
+export const AuthContext = createContext<AuthContextType | undefined>(undefined);
